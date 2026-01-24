07d55339cfb1a43e1fd0964586721c8aea26cf9e
Fastco Dev <dev@fastco.com>
2026-01-24 02:45:17 +0000

    Sync fastresp-server-web updates

--- web/js/brand.js
+++ web/js/brand.js
@@ -1,19 +1,17 @@
 $(document).ready(function () {
     const link = new LinkRef('page-link', 'brand-section');
-    document.addEventListener('DOMContentLoaded', () => {
-        const headDetailBox = document.querySelector('.page-header-desc-right-detail-box');
-        const text = headDetailBox.querySelector('.page-header-desc-right-detail-text');
-        const button = headDetailBox.querySelector('.page-header-desc-right-detail-toggle-btn');
-        button.addEventListener('click', function () {
-            const isToggle = text.classList.contains('is-toggle');
-            if (isToggle) {
-                text.classList.remove('is-toggle');
-                button.innerHTML = 'Show Detail';
-            } else {
-                text.classList.add('is-toggle');
-                button.innerHTML = 'Show Less';
-            }
-        })
+    const headDetailBox = document.querySelector('.page-header-desc-right-detail-box');
+    const text = headDetailBox.querySelector('.page-header-desc-right-detail-text');
+    const button = headDetailBox.querySelector('.page-header-desc-right-detail-toggle-btn');
+    button.addEventListener('click', function () {
+        const isToggle = text.classList.contains('is-toggle');
+        if (isToggle) {
+            text.classList.remove('is-toggle');
+            button.innerHTML = 'Show Detail';
+        } else {
+            text.classList.add('is-toggle');
+            button.innerHTML = 'Show Less';
+        }
     })
     $('.faq-header-arrow-icon').on('click', e => {
         $(e.target).toggleClass('open')
